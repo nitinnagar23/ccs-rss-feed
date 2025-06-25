2cb1b09c6f5e0e1e356f4d414bb5c8be91693714
nitinnagar23 <nitinnagar43@gmail.com>
2025-06-25 17:10:23 +0530

    Update generate_rss.py

--- generate_rss.py
+++ generate_rss.py
@@ -3,29 +3,36 @@ from bs4 import BeautifulSoup
 from feedgen.feed import FeedGenerator
 from datetime import datetime
 
-URL = 'https://www.ccsuniversity.ac.in/search-news?title=&category=&month=&year=&page=1'
 BASE = 'https://www.ccsuniversity.ac.in'
+NEWS_URL = BASE + '/search-news?title=&category=&month=&year=&page=1'
 
-res = requests.get(URL)
+res = requests.get(NEWS_URL)
 soup = BeautifulSoup(res.text, 'html.parser')
 
 fg = FeedGenerator()
 fg.title('CCS University - Latest News')
-fg.link(href=URL)
+fg.link(href=NEWS_URL)
 fg.description('News updates from CCS University Meerut')
 fg.language('en')
 
 for item in soup.select('.news-detail'):
     title_tag = item.select_one('h5')
     link_tag = item.select_one('a')
-    date_tag = item.select_one('.date')  # Adjust this selector based on actual structure
+    date_tag = item.select_one('.date')
 
     if not (title_tag and link_tag and date_tag):
         continue
 
     title = title_tag.get_text(strip=True)
-    link = BASE + link_tag['href']
+    detail_href = BASE + link_tag['href']
     date_text = date_tag.get_text(strip=True)
+
+    # Try to get redirected PDF URL
+    try:
+        detail_res = requests.get(detail_href, allow_redirects=True)
+        final_url = detail_res.url  # Will be cdn.ccsuniversity.ac.in if redirect happens
+    except:
+        final_url = detail_href  # Fallback to detail page if request fails
 
     try:
         pub_date = datetime.strptime(date_text, '%d-%m-%Y')
@@ -34,7 +41,7 @@ for item in soup.select('.news-detail'):
 
     fe = fg.add_entry()
     fe.title(title)
-    fe.link(href=link)
+    fe.link(href=final_url)
     fe.pubDate(pub_date)
 
-fg.rss_file('ccs-feed.xml')
+fg.rss_file('ccs-feed.xml', encoding='utf-8')
