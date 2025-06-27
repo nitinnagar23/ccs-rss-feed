c4db87d2b64c573821ae1ae0e1db1310657ae4df
nitinnagar23 <nitinnagar43@gmail.com>
2025-06-27 08:54:45 +0530

    Update generate_rss.py

--- generate_rss.py
+++ generate_rss.py
@@ -3,45 +3,55 @@ from bs4 import BeautifulSoup
 from feedgen.feed import FeedGenerator
 from datetime import datetime
 
-BASE = 'https://www.ccsuniversity.ac.in'
-NEWS_URL = BASE + '/search-news?title=&category=&month=&year=&page=1'
+URL = "https://www.ccsuniversity.ac.in/search-news?title=&category=&month=&year=&page=1"
+BASE_URL = "https://www.ccsuniversity.ac.in/"
 
-res = requests.get(NEWS_URL)
-soup = BeautifulSoup(res.text, 'html.parser')
+def fetch_notices():
+    response = requests.get(URL)
+    soup = BeautifulSoup(response.content, "html.parser")
+    
+    # Find the table containing the links
+    table = soup.find("table")
+    items = []
 
-fg = FeedGenerator()
-fg.title('CCS University - Latest News')
-fg.link(href=NEWS_URL)
-fg.description('News updates from CCS University Meerut')
-fg.language('en')
+    if table:
+        for a in table.find_all("a", href=True):
+            title = a.get_text(strip=True)
+            link = a["href"]
 
-for item in soup.select('.news-detail'):
-    title_tag = item.select_one('h5')
-    link_tag = item.select_one('a')
-    date_tag = item.select_one('.date')
+            # Skip empty titles or links
+            if not title or not link:
+                continue
 
-    if not (title_tag and link_tag and date_tag):
-        continue
+            # Handle relative URLs just in case
+            if not link.startswith("http"):
+                link = BASE_URL + link.lstrip("/")
 
-    title = title_tag.get_text(strip=True)
-    detail_href = BASE + link_tag['href']
-    date_text = date_tag.get_text(strip=True)
+            items.append({
+                "title": title,
+                "link": link,
+                "pubDate": datetime.utcnow().strftime("%a, %d %b %Y %H:%M:%S +0000"),
+                "guid": link,
+            })
+    return items
 
-    # Follow redirect to actual PDF if it exists
-    try:
-        detail_res = requests.get(detail_href, allow_redirects=True, timeout=10)
-        final_url = detail_res.url if "cdn.ccsuniversity.ac.in" in detail_res.url else detail_href
-    except:
-        final_url = detail_href
+def generate_rss():
+    fg = FeedGenerator()
+    fg.title("CCS University - Latest News")
+    fg.link(href=URL, rel='alternate')
+    fg.description("News updates from CCS University Meerut")
+    fg.language("en")
+    fg.lastBuildDate(datetime.utcnow())
+    fg.link(href="https://nitinnagar23.github.io/ccs-rss-feed/ccs-feed.xml", rel="self", type="application/rss+xml")
 
-    try:
-        pub_date = datetime.strptime(date_text, '%d-%m-%Y')
-    except:
-        pub_date = datetime.utcnow()
+    for item in fetch_notices():
+        fe = fg.add_entry()
+        fe.title(item["title"])
+        fe.link(href=item["link"])
+        fe.guid(item["guid"])
+        fe.pubDate(item["pubDate"])
 
-    fe = fg.add_entry()
-    fe.title(title)
-    fe.link(href=final_url)
-    fe.pubDate(pub_date)
+    fg.rss_file("ccs-feed.xml")
 
-fg.rss_file('ccs-feed.xml', encoding='utf-8')
+if __name__ == "__main__":
+    generate_rss()
